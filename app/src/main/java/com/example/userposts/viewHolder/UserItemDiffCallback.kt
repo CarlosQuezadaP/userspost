77cee95711f27f6b550dc0ad8bf4eba5b1271644
Carlos Quezada <cquezada@condorlabs.io>
2021-06-02 22:31:04 -0500

    Cambio metodo de busqueda

--- app/src/main/java/com/example/userposts/viewHolder/UserItemDiffCallback.kt
+++ app/src/main/java/com/example/userposts/viewHolder/UserItemDiffCallback.kt
@@ -5,11 +5,11 @@ import com.example.dominio.model.UserDomain
 
 class UserItemDiffCallback : DiffUtil.ItemCallback<UserDomain>() {
     override fun areItemsTheSame(oldItem: UserDomain, newItem: UserDomain): Boolean {
-        return oldItem == newItem
+        return oldItem.id == newItem.id
     }
 
     override fun areContentsTheSame(oldItem: UserDomain, newItem: UserDomain): Boolean {
-        return oldItem.id == newItem.id
+        return oldItem.posts == newItem.posts
     }
 
 }
