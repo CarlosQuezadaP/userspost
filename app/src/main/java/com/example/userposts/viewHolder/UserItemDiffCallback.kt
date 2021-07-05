8375e26a210ad76f8310c180bd3cd76e0cebbb48
Carlos Quezada <cquezada@condorlabs.io>
2021-07-04 23:18:50 -0500

    add equals function

--- app/src/main/java/com/example/userposts/viewHolder/UserItemDiffCallback.kt
+++ app/src/main/java/com/example/userposts/viewHolder/UserItemDiffCallback.kt
@@ -4,14 +4,13 @@ import android.annotation.SuppressLint
 import androidx.recyclerview.widget.DiffUtil
 import com.example.dominio.model.UserDomain
 
-@SuppressLint("DiffUtilEquals")
 class UserItemDiffCallback : DiffUtil.ItemCallback<UserDomain>() {
     override fun areItemsTheSame(oldItem: UserDomain, newItem: UserDomain): Boolean {
         return oldItem.id == newItem.id
     }
 
     override fun areContentsTheSame(oldItem: UserDomain, newItem: UserDomain): Boolean {
-        return oldItem == newItem
+        return oldItem.equals(newItem)
     }
 
 }
